53f4ab7ea00498cbe50225971a532657e1baec2e
elisabichiappini <elisabichiappini@gmail.com>
2025-11-29 15:48:49 +0100

    feat: random funzione

--- app.js
+++ app.js
@@ -7,6 +7,6 @@
 //         reject('no')
 //     }
 // })
-
+//math.random : randomizza un numero da 0 a 1 con virgola, e math.round : arrotonda 
 const isConnectToServer = Math.round(Math.random());
-console.log(isConnectToServer);
+console.log(isConnectToServer); //comando npm run dev
