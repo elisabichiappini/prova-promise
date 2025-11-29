812488a50f52e2d2e6ba06fd4f9b66bff9d80431
elisabichiappini <elisabichiappini@gmail.com>
2025-11-29 15:59:27 +0100

    feat: fattore temporale

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 
 const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+
     const isConnectToServer = Math.round(Math.random());
     if(isConnectToServer) {
         resolve('ok sei connesso')
     } else {
         reject('non sei connesso!!!!')
-    }
+    }}, 2000)
+
 })
 promise.then(mess => console.log(mess))
 .catch(err => console.error('ERROR:', err))
