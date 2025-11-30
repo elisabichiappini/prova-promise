3b10ccbbcf55d3c7e85893b2d2a68df3a2fbc982
elisabichiappini <elisabichiappini@gmail.com>
2025-11-30 11:29:42 +0100

    feat: lettura json file

--- app.js
+++ app.js
@@ -1,53 +1,15 @@
+const path = require('path');
+const fs = require('fs');
 
-// const promise = new Promise((resolve, reject) => {
-//     const randomTime = Math.round(Math.random() * 4000);
-//     let checkTimeout, checkServer;
+async function loadJsonData(nomeFile) {
+    const filePath = path.join(__dirname, nomeFile);
+    const data = await fs.promises.readFile(filePath);
+    return JSON.parse(data);
+}
 
-//     checkTimeout = setTimeout(() => {
-//         reject('500 server offline');
-//         clearTimeout(checkServer);
-//     }, 200);
+async function main() {
+    const data = await loadJsonData('data.json');
+    console.log(data);
+}
 
-//     checkServer = setTimeout(() => {
-//  const isConnectToServer = Math.round(Math.random());
-//     if(isConnectToServer) {
-//         resolve('ok sei connesso')
-//     } else {
-//         reject('non sei connesso!!!!')
-//     }
-//     clearTimeout(checkTimeout);
-//     }, randomTime)
-// })
-
-
-// promise.then(mess => console.log(mess))
-// .catch(err => console.error('ERROR:', err))
-
-
-//prova con fetch!
-
-const fetch = (url) => new Promise((resolve, reject) => {
-    const randomTime = Math.round(Math.random() * 4000);
-    let checkTimeout, checkServer;
-
-    checkTimeout = setTimeout(() => {
-        reject(new Error('500 server offline'));
-        clearTimeout(checkServer);
-    }, 2000);
-
-    checkServer = setTimeout(() => {
-        const isConnectToServer = Math.round(Math.random());
-        if (isConnectToServer) {
-            resolve(new Response('ok sei connesso'))
-        } else {
-            reject(new Error('non sei connesso!!!!'))
-        }
-        clearTimeout(checkTimeout);
-    }, randomTime);
-})
-
-
-fetch('http://blaVla.com')
-    .then(mess => console.log(mess))
-    .catch(err => console.error('ERROR:', err));
-//npm run dev
+main();
